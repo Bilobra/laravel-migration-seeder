a8f79fed37aab83d4beec675200342397eaaed6c
Bilobra <bra.carrozzo@gmail.com>
2022-10-12 15:36:46 +0200

    add seeder array

--- database/migrations/2022_10_12_124832_create_trains_table.php
+++ database/migrations/2022_10_12_124832_create_trains_table.php
@@ -18,8 +18,8 @@ class CreateTrainsTable extends Migration
             $table->string('azienda',50);
             $table->string('stazione_di_partenza', 50);
             $table->string('stazione_di_arrivo', 50);
-            $table->time('orario_di_partenza', 4);
-            $table->time('orario_di_arrivo', 4);
+            $table->time('orario_di_partenza');
+            $table->time('orario_di_arrivo');
             $table->string('codice_treno',50)->unique();
             $table->unsignedTinyInteger('numero_carrozze')->default(100);
             $table->unsignedTinyInteger('in_orario')->default(1);
--- database/seeds/TrainSeeder.php
+++ database/seeds/TrainSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Train;
 use Illuminate\Database\Seeder;
 
 class TrainSeeder extends Seeder
@@ -11,6 +12,61 @@ class TrainSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //creo array associativo
+        $trains = [
+            [
+                'azienda' => 'Trenitalia',
+                'stazione_di_partenza' => 'Novara',
+                'stazione_di_arrivo' => 'Milano',
+                'orario_di_partenza' => '12:00:00',
+                'orario_di_arrivo' => '13:30:00',
+                'codice_treno' => 'FGK67DS',
+                'numero_carrozze' => 68,
+                'in_orario' => 1,
+                'cancellato' => 0,
+            ],
+            [
+                'azienda' => 'Trenord',
+                'stazione_di_partenza' => 'Galliate',
+                'stazione_di_arrivo' => 'Cadorna',
+                'orario_di_partenza' => '10:00:00',
+                'orario_di_arrivo' => '11:30:00',
+                'codice_treno' => 'FGKaop3',
+                'numero_carrozze' => 40,
+                'in_orario' => 0,
+                'cancellato' => 0,
+            ],
+            [
+                'azienda' => 'Trenitalia',
+                'stazione_di_partenza' => 'Arona',
+                'stazione_di_arrivo' => 'Bellinzago',
+                'orario_di_partenza' => '22:50:00',
+                'orario_di_arrivo' => '23:30:00',
+                'codice_treno' => 'H4DKaop3',
+                'numero_carrozze' => 20,
+                'in_orario' => 1,
+                'cancellato' => 0,
+            ],
+        ];
+
+        // ciclo array con foeach
+        // creo instanza vuota della classe Train(model)
+        // valorizzo le proprietà dell'istanza
+        // salvo con ->save() nel DB
+
+        foreach ($trains as $train) {
+            $newTrain = new Train();
+            $newTrain->azienda = $train['azienda'];
+            $newTrain->stazione_di_partenza = $train['stazione_di_partenza'];
+            $newTrain->stazione_di_arrivo = $train['stazione_di_arrivo'];
+            $newTrain->orario_di_partenza = $train['orario_di_partenza'];
+            $newTrain->orario_di_arrivo = $train['orario_di_arrivo'];
+            $newTrain->codice_treno = $train['codice_treno'];
+            $newTrain->numero_carrozze = $train['numero_carrozze'];
+            $newTrain->in_orario = $train['in_orario'];
+            $newTrain->cancellato = $train['cancellato'];
+
+            $newTrain->save();
+        }
     }
 }
